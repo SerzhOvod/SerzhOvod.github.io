9ff77902a17c8f14f385679ecb7fa8e340be2f5f
Sergey Ovodenko <sergey.ovodenko@gmail.com>
2023-03-14 19:03:33 +0200

    home

--- src/sendmail.php
+++ src/sendmail.php
@@ -11,7 +11,7 @@ $mail->CharSet = 'UTF-8';
 $mail->setLanguage('en', 'phpmailer/language/');
 $mail->IsHTML(true);
 
-/*
+
 	$mail->isSMTP();                                            //Send using SMTP
 	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
 	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -19,7 +19,7 @@ $mail->IsHTML(true);
 	$mail->Password   = 'secret';                               //SMTP password
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 	$mail->Port       = 465;                 
-	*/
+	
 
 //Від кого лист
 $mail->setFrom($_POST['email'], 'New customer'); // Вказати потрібний E-mail
